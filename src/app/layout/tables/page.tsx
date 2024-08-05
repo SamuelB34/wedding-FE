b2cb1b0a6670116de5f2ffc10ea2c4cc55c9c9bf
marlene-araiza <marlene_araiza@outlook.com>
2024-08-04 20:07:58 -0700

    Create tables stylesheet

--- src/app/layout/tables/page.tsx
+++ src/app/layout/tables/page.tsx
@@ -1,3 +1,21 @@
+import styles from ".tables.module.scss";
+
 export default function Tables() {
-  return <>Tables Page</>;
+  return <> 
+    <div className="Top">
+        <nav></nav>
+        <h1 className="Logo">Sam-Mel</h1>
+        <span></span>
+    </div>
+    <div className="Title">
+        <h2>Tables</h2>
+        <p>10 tables full</p>
+    </div>
+    <div className="Layout">
+        <p>List</p>
+        <p>Floor Plan</p>
+    </div>
+  
+  </>;
+
 }
