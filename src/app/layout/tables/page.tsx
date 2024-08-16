cf75a906d465ea3aab9a2524ba99f9fc5bead9bc
Samuel Barragan <samuel.barragan3417@gmail.com>
2024-08-15 21:51:51 -0700

    Add comments to WebGuest component

--- src/app/layout/tables/page.tsx
+++ src/app/layout/tables/page.tsx
@@ -1,9 +1,44 @@
+"use client";
 import { WebTables } from "@/shared/components/web-tables/WebTables";
+import { WebGuest } from "@/shared/components/web-guest/WebGuest";
 
 export default function Tables() {
+  const guestLis = [
+    {
+      id: "abcd1234",
+      full_name: "John Doe",
+      group_name: "Doe Family",
+      table_name: null,
+    },
+    {
+      id: "abcd1235",
+      full_name: "John Doe",
+      group_name: "Doe Family",
+      table_name: null,
+    },
+    {
+      id: "abcd1236",
+      full_name: "John Doe",
+      group_name: "Doe Family",
+      table_name: null,
+    },
+  ];
+
   return (
     <>
       <WebTables records={10} loading={false} />
+
+      {guestLis.map((guest) => {
+        return (
+          <WebGuest
+            key={guest.id}
+            guest={guest}
+            onClickSeatGuest={(id) => {
+              console.log(id);
+            }}
+          />
+        );
+      })}
     </>
   );
 }
--- src/shared/components/web-guest/WebGuest.tsx
+++ src/shared/components/web-guest/WebGuest.tsx
@@ -3,60 +3,47 @@ import styles from "./web-guest.module.scss";
 import WebButton from "../wed-button/WebButton";
 
 interface Props {
+  // Aqui fue mi error, no tenia que ser array, una disculpa jeje
   guest: {
     id: string;
     full_name: string;
     group_name: string | null;
     table_name: string | null;
-  }[];
+  };
   onClickSeatGuest: (id: string) => void;
 }
 
 export function WebGuest({ guest, onClickSeatGuest }: Props) {
   return (
     <div className={styles.container}>
-      {
-        // Antes estaba con un signo de interrogacion, lo que da a entender que puede que haya
-        // ocaciones en las que no vengan "guests", pero en este caso son requeridos, entonces
-        // no es necesario agregar esa condicional
-        // guest?.map((guest: {
-        guest.map(
-          (guest: {
-            id: string;
-            full_name: string;
-            group_name: string | null;
-            table_name: string | null;
-          }) => {
-            return (
-              <div key={guest.id}>
-                <div className={styles.left}>
-                  <span className={styles.full_name}>{guest.full_name}</span>
-                  <span className={styles.group_name}>
-                    {guest.group_name ? guest.group_name : "No group assigned"}
-                  </span>
-                </div>
+      <div className={styles.left}>
+        <div className={styles.left__name}>
+          {/*Habia hecho falta agregar esto de aqui*/}
+          <div className={styles.initial}>
+            {/*Aqui le digo que tome la primera letra del nombre*/}
+            <span>{guest.full_name.charAt(0)}</span>
+          </div>
+          <span className={styles.full_name}>{guest.full_name}</span>
+        </div>
+        <span className={styles.group_name}>
+          {guest.group_name ? guest.group_name : "No group assigned"}
+        </span>
+      </div>
 
-                <div></div>
-
-                <div className={styles.right}>
-                  <span className={styles.table_name}>
-                    {guest.table_name ? guest.table_name : "No table assigned"}
-                  </span>
-                  <WebButton
-                    type={"submit"}
-                    style={"basic"}
-                    onClick={() => {
-                      if (onClickSeatGuest) onClickSeatGuest(guest.id);
-                    }}
-                  >
-                    <>Seat Guest</>
-                  </WebButton>
-                </div>
-              </div>
-            );
-          },
-        )
-      }
+      <div className={styles.right}>
+        <span className={styles.table_name}>
+          {guest.table_name ? guest.table_name : "No table assigned"}
+        </span>
+        <WebButton
+          type={"submit"}
+          style={"basic"}
+          onClick={() => {
+            if (onClickSeatGuest) onClickSeatGuest(guest.id);
+          }}
+        >
+          <>Seat Guest</>
+        </WebButton>
+      </div>
     </div>
   );
 }
